a94311f11ca740516f2f421ac4ba1121274074c0
Agustin Pina <aguspina87@gmail.com>
2018-04-13 12:32:23 -0300

    Making code more neat

--- lib/checker/css.rb
+++ lib/checker/css.rb
@@ -1,3 +1,9 @@
+class CssParser::Parser
+  def tree
+    to_h['all'].with_indifferent_access
+  end
+end
+
 module Checker
   class CSS
     def self.run(document, expectation, binding)
@@ -5,13 +11,21 @@ module Checker
       inspection = expectation.inspection
       parser = CssParser::Parser.new
       parser.load_string! content
-      target = inspection.target
-      raise 'Target is required' if target.blank?
+      raise 'Target is required' if inspection.target.blank?
       case inspection.type
-        when 'DeclaresTag'       then parser.to_h['all'][target.to_s].present?
-        when 'DeclaresAttribute' then parser.to_h['all'][binding.to_s][target.to_s.split(':').first] == target.to_s.split(':').last
+        when 'DeclaresTag'       then inspect_tag(parser, inspection)
+        when 'DeclaresAttribute' then inspect_attribute(parser, inspection, binding)
         else raise "Unsupported inspection #{inspection.type}"
       end
+    end
+
+    def self.inspect_tag(parser, inspection)
+      parser.tree[inspection.target.to_s].present?
+    end
+
+    def self.inspect_attribute(parser, inspection, binding)
+      property, value = inspection.target.to_s.split(':')
+      parser.tree[binding.to_s][property] == value
     end
   end
 end
