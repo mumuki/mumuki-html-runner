47efcf3a8af8291fc54e969b429de8076fd4e3cc
Rodrigo Alfonso <rodri042@gmail.com>
2018-11-21 01:02:41 -0300

    Moving sub-hooks to instance variables

--- lib/test_hook.rb
+++ lib/test_hook.rb
@@ -2,17 +2,20 @@ require_relative './test_dom_hook'
 require_relative './test_script_hook'
 
 class HtmlTestHook < Mumukit::Hook
-  TEST_DOM_HOOK = HtmlTestDomHook.new
-  TEST_SCRIPT_HOOK = HtmlTestScriptHook.new
+  def initialize(config = nil)
+    super config
+    @dom_hook = HtmlTestDomHook.new
+    @script_hook = HtmlTestScriptHook.new
+  end
 
   def compile(request)
     request = struct request.to_h.merge options: options(request)
-    TEST_SCRIPT_HOOK.compile TEST_DOM_HOOK.compile(request)
+    @script_hook.compile @dom_hook.compile(request)
   end
 
   def run!(request)
-    dom_output, dom_status = TEST_DOM_HOOK.run! request
-    script_results = TEST_SCRIPT_HOOK.run!(request)
+    dom_output, dom_status = @dom_hook.run! request
+    script_results = @script_hook.run!(request)
     return ["<pre>#{script_results&.last}</pre>", :errored] if script_results&.first == :errored
     script_test_results = script_results&.first
 
